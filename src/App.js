97de4c885f8312f9c4e23a0c61109ecdd57a7d02
김남규 <nick1961@naver.com>
2023-06-01 15:59:58 +0900

    0601

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집', '파이썬 독학']);
   let [따봉,따봉변경] = useState([0,0,0]); 
   let [modal,setModal] = useState(false);
+06  let [selectedTitle,setSelectedTitle] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -39,7 +41,8 @@ function App() {
           return (
           <div className="list" key={i}>
              <h4 onClick={()=>{
-                setModal(!modal , 글제목[i]);
+                setSelectedTitle(i);
+                setModal(!modal);
               }}> { 글제목[i] } 
               <span onClick={ ()=>{
                   let copy = [...따봉];
@@ -53,7 +56,7 @@ function App() {
         })
       }
       {
-        modal ? <Modal titleChang={글제목변경} title={글제목} selected/> : '' 
+        modal ? <Modal titleChang={글제목변경} title={글제목} modalTitle={selectedTitle}/> : '' 
       }
          
     </div>
@@ -64,23 +67,14 @@ function Modal(props){
   
   return (
     <div className="modal">
-      {
-        props.title.map(function(name,idx){
-          return(
-            <>
-            <h4>{name}</h4>
-            <p>날짜</p>
-            <p>상세내용</p>
-            <button onClick={ () => {
-              let copy = [...props.title];
-              copy[0] = '여자 코트 추천';
-              props.titleChang(copy);
-            }}>글수정</button>
-            </>
-          );
-        })
-      }
-        
+      <h4>{props.title[props.modalTitle]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={ () => {
+        let copy = [...props.title];
+        copy[0] = '여자 코트 추천';
+        props.titleChang(copy);
+      }}>글수정</button>
     </div>
   );
 }
